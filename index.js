ace2d8805baf4baddce6eca288bee7ce921a4431
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2025-02-16 08:13:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PAGE_ACCESS_TOKEN = process.env.token;
 
 const chatGpt = async (text, senderId) => {
   try {
-    const repl = `[Prompt: Never use LaTex and text formatting]\n\n${text}`;
+    const repl = `[Prompt: Never use LaTex for formatting formules]\n\n${text}`;
     const response = await axios.get(`https://kaiz-apis.gleeze.com/api/pixtral-12b?q=${encodeURIComponent(repl)}&uid=${senderId}`);
 
     const reply = response.data.content;
