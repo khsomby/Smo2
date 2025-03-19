14f8f72161035d168b74c9396f68a0c92a8ba460
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2025-03-19 19:07:30 +0300

    Add new file: emojimix.js to /commands/ directory

---
+++ commands/emojimix.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
+const description = `/emojimix <emoji1> <emoji2>
+Example: /emojimix 😀 😘`;
+
+module.exports = {
+  description,
+  execute: async (prompt, senderId, sendMessage) => {
+    if (!prompt) {
+      return sendMessage(senderId, { text: "❌ Please provide emojis after /emojimix" });
+    }
+
+    try {
+      const [e1, e2] = prompt.split(' ').map(item => item.trim());
+
+      const imgUrl = `https://betadash-api-swordslush-production.up.railway.app/emojimix?emoji1=${e1}&emoji2=${e2}`;
+  
+      await sendMessage(senderId, {
+        attachment: { type: "image", payload: { url: imgUrl, is_reusable: true } }
+      });
+    } catch (error) {
+      console.error("Error:", error.message);
+      sendMessage(senderId, { text: "❌ Error generating image." });
+    }
+  }
+};
