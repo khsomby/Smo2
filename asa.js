30d86e0fab5f695c63bda78c699a30770bb52806
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2025-05-23 19:27:42 +0300

    Update asa.js

--- asa.js
+++ asa.js
@@ -6,9 +6,8 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-// Configuration
 const PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
-const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+const VERIFY_TOKEN = "somby";
 const API_VERSION = 'v18.0';
 
 const activePosts = {};
