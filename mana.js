71b59f537e88f85ca1d83b19066e4b82964ec764
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2025-06-07 09:52:54 +0300

    Update mana.js

--- mana.js
+++ mana.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 const app = express();
 
