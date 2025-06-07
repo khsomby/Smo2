9255302727bb2f71205ebef419182a89dff011ad
Somby Ny Aina <137616969+khsomby@users.noreply.github.com>
2025-06-07 08:57:33 +0300

    Update mana.js

--- mana.js
+++ mana.js
@@ -16,6 +16,159 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 app.use(express.static('public'));
 
+
+let createdAccounts = [];
+
+const genRandomString = (length) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+};
+
+// Random date of birth
+const getRandomDate = (start, end) => {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+
+// Random Malagasy name generator
+const getRandomName = () => {
+  const firstNames = [
+    'Toky', 'Fara', 'Mamy', 'Lova', 'Hery', 'Niry', 'Soa', 'Bema', 'Ony', 'Lala',
+    'Hasina', 'Rina', 'Kanto', 'Fanja', 'Tantely', 'Zo', 'Voahirana', 'Ny Aina', 'Malala', 'Tahina',
+    'Fitiavana', 'Mahery', 'Mialy', 'Nomenjanahary', 'Tsanta'
+  ];
+
+  const lastNames = [
+    'Rakoto', 'Rasoanaivo', 'Randriamihaja', 'Andrianarisoa', 'Rasoa', 'Razafindrakoto',
+    'Rakotondrazaka', 'Randrianarivelo', 'Raharimanana', 'Andrianantenaina',
+    'Rabe', 'Rasoazanany', 'Rakotomalala', 'Razanadrakoto', 'Andriamanantena',
+    'Ramananoro', 'Razafindrahaga', 'Randrianasolo', 'Ralison', 'Andriambelo',
+    'Razanakolona', 'Randriambololona', 'Rakotovao', 'Rasolonjatovo', 'Andrianasolo'
+  ];
+
+  const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+  const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+  return { firstName, lastName };
+};
+
+// Fetch email domains from mail.tm
+const getMailDomains = async () => {
+  try {
+    const res = await axios.get('https://api.mail.tm/domains');
+    return res.data['hydra:member'];
+  } catch {
+    return null;
+  }
+};
+
+// Create temporary email account
+const createMailTmAccount = async () => {
+  const domains = await getMailDomains();
+  if (!domains) return null;
+
+  const domain = domains[Math.floor(Math.random() * domains.length)].domain;
+  const username = genRandomString(10);
+  const password = genRandomString(12);
+  const birthday = getRandomDate(new Date(1976, 0, 1), new Date(2004, 0, 1));
+  const { firstName, lastName } = getRandomName();
+
+  try {
+    const res = await axios.post('https://api.mail.tm/accounts', {
+      address: `${username}@${domain}`,
+      password
+    });
+
+    if (res.status === 201) {
+      return { email: `${username}@${domain}`, password, firstName, lastName, birthday };
+    }
+  } catch {
+    return null;
+  }
+};
+
+// Register account to Facebook
+const registerFacebookAccount = async (email, password, firstName, lastName, birthday) => {
+  const api_key = '882a8490361da98702bf97a021ddc14d';
+  const secret = '62f8ce9f74b12f84c123cc23437a4a32';
+  const gender = Math.random() < 0.5 ? 'M' : 'F';
+
+  const req = {
+    api_key,
+    attempt_login: true,
+    birthday: birthday.toISOString().split('T')[0],
+    client_country_code: 'EN',
+    fb_api_caller_class: 'com.facebook.registration.protocol.RegisterAccountMethod',
+    fb_api_req_friendly_name: 'registerAccount',
+    firstname: firstName,
+    format: 'json',
+    gender,
+    lastname: lastName,
+    email,
+    locale: 'en_US',
+    method: 'user.register',
+    password,
+    reg_instance: genRandomString(32),
+    return_multiple_errors: true,
+  };
+
+  const sigString = Object.keys(req).sort().map(k => `${k}=${req[k]}`).join('') + secret;
+  req.sig = crypto.createHash('md5').update(sigString).digest('hex');
+
+  try {
+    const res = await axios.post('https://b-api.facebook.com/method/user.register', new URLSearchParams(req), {
+      headers: {
+        'User-Agent': '[FBAN/FB4A;FBAV/35.0.0.48.273;FBDM/{density=1.33125,width=800,height=1205};FBLC/en_US;FBCR/;FBPN/com.facebook.katana;FBDV/Nexus 7;FBSV/4.1.1;FBBK/0;]'
+      }
+    });
+
+    return res.data;
+  } catch {
+    return null;
+  }
+};
+
+// POST /create
+app.post("/create", async (req, res) => {
+  const amount = parseInt(req.body.amount);
+  if (!amount || amount <= 0) {
+    return res.status(400).json({ error: "Invalid 'amount'" });
+  }
+
+  const results = [];
+
+  for (let i = 0; i < amount; i++) {
+    const emailAccount = await createMailTmAccount();
+    if (!emailAccount) continue;
+
+    const fb = await registerFacebookAccount(
+      emailAccount.email,
+      emailAccount.password,
+      emailAccount.firstName,
+      emailAccount.lastName,
+      emailAccount.birthday
+    );
+
+    if (fb && fb.new_user_id) {
+      results.push({
+        email: emailAccount.email,
+        password: emailAccount.password,
+        name: `${emailAccount.firstName} ${emailAccount.lastName}`,
+        birthday: emailAccount.birthday.toISOString().split("T")[0],
+        gender: fb.gender,
+        userId: fb.new_user_id,
+        token: fb.session_info?.access_token || null,
+      });
+    }
+  }
+
+  createdAccounts = createdAccounts.concat(results);
+  res.json({ created: results.length, accounts: results });
+});
+
+app.get("/list", (req, res) => {
+  res.json(createdAccounts);
+});
+
+
 // Multer setup for uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
